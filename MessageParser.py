fb7803463a8df34e5406506854b9851b53098cca
mathiaga <mathias.gallefoss@gmail.com>
2017-03-21 15:04:41 +0100

    Update MessageParser.py

--- MessageParser.py
+++ MessageParser.py
@@ -1,4 +1,4 @@
-
+import json
 
 class MessageParser():
     def __init__(self):
@@ -6,19 +6,37 @@ class MessageParser():
         self.possible_responses = {
             'error': self.parse_error,
             'info': self.parse_info,
-	    # TODO: More key:values pairs are needed	
+            'message': self.parse_message,
+            'history': self.parse_history
         }
 
     def parse(self, payload):
-        payload = # TODO:decode the JSON object
+        payload = json.loads(payload) # TODO:decode the JSON object
 
         if payload['response'] in self.possible_responses:
             return self.possible_responses[payload['response']](payload)
         else:
-            # TODO: Response not valid
+            print payload
+            print 'Not valid'
+            return 'invalid'
 
     def parse_error(self, payload):
+		print 'Error'
+		print payload['content']
+		return 'error'
     
-    def parse_info(self, payload):
+def parse_info(self, payload):
+        print payload['content']
+        return payload['content']
     
-    #TODO: Include more methods for handling the different responses... 
+    def parse_message(self, payload):
+	print payload['timestamp'],' - ',payload['sender'], ': ',payload['content']
+        return 'message'
+
+    def parse_history(self, payload):
+        print 'Previously sent messages: '
+        messageList = payload['content']
+        for elem in messageList:
+            message=json.loads(elem)
+            print message['timestamp'],' - ',message['sender'], ': ', message['content']
+        return 'history'
